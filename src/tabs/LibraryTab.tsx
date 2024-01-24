c4d668f2237a701455de828207384b34808c3bbd
al1sah1l <alisahil.us@gmail.com>
2024-01-24 01:01:42 -0800

    add story button library

--- src/tabs/LibraryTab.tsx
+++ src/tabs/LibraryTab.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import '../App.css';
 import {useSelector} from "react-redux";
-import {RootState} from "../store";
+import {AppDispatch, RootState} from "../store";
 import {StoryType} from "../types";
-import {Col, Row, Tabs} from "antd";
+import {Button, Col, Row, Tabs} from "antd";
 import {StoryCard} from "../components/storyCard";
 import Title from "antd/lib/typography/Title";
+import {CloseOutlined} from "@ant-design/icons";
+import {v4 as uuidv4} from "uuid";
+import {setCurrentStory} from "../store/slices/appSlice";
+import {useDispatch} from "react-redux";
 
 type TargetKey = React.MouseEvent | React.KeyboardEvent | string;
 
 export default function LibraryTab() {
     const libraryStory = useSelector<RootState, StoryType[]>((state) => state.app.libraryStory!);
     const currentStory = useSelector<RootState, StoryType>((state) => state.app.currentStory!);
+    const dispatch = useDispatch<AppDispatch>();
+
+    const onClickCreateNew = () => {
+        const newStory: StoryType= {
+            id: uuidv4(),
+            textFromVoice: '',
+            name: '',
+            characters: [],
+            charactersNames: [],
+            story: ''
+        }
+        dispatch(setCurrentStory(newStory));
+        // add switching to Generator Tab
+    }
 
     return (
         <div>
@@ -21,6 +39,9 @@ export default function LibraryTab() {
                     {libraryStory.map((story) => {
                         return <StoryCard key={story.id} story={story}/>
                     })}
+                    <Button onClick={onClickCreateNew}>
+                        <CloseOutlined />
+                    </Button>
                 </Col>
                 <Col span={18} style={{padding: 20}}>
                     <p>{currentStory.story}</p>
